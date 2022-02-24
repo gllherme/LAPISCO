b6dfb0f34c1371b818951dcbb92d9819ae80998f
gllherme <guicampos159@gmail.com>
2022-02-24 15:26:00 -0300

    front and API

---
+++ script.js
@@ -0,0 +1,59 @@
+// Declara uma classe contendo as informações do usuario
+class User {
+    constructor(name, gender, cellPhone, email, age, imageURL) {
+        this.name = name
+        this.gender = gender
+        this.cellPhone = cellPhone
+        this.email = email
+        this.age = age;
+        this.imageURL = imageURL;
+    }
+}
+
+// Obtem os dados da API
+async function getUserData(numberOfElements) {
+    const response = await fetch(`https://randomuser.me/api/?results=${numberOfElements}`)
+    const result = await response.json()
+    return result["results"]
+}
+
+// Carrega e filtra os dados da API
+async function loadUserData(numberOfElements) { 
+    let userDataJson = await getUserData(numberOfElements)
+    
+    userDataJson.forEach(user => {
+        const name = `${user["name"]["first"]} ${user["name"]["last"]}`
+        const gender = user["gender"]
+        const cellPhone = user["cell"]
+        const email = user["email"]
+        const age = user["registered"]["age"]
+        const imageURL = user["picture"]["medium"]
+        const filteredUserData = new User(name, gender, cellPhone, email, age, imageURL);
+        userData.push(filteredUserData)
+    });
+    console.log(userData)
+    renderUserCard(userData)
+}
+
+let userData = []
+
+// Cria um elemento HTML contendo as informações geradas pela API
+function renderUserCard(userData) {
+    userData.forEach(user => {
+        const card = document.createElement('div')
+        console.log(card)
+        card.classList.add('user-card')
+        card.innerHTML = `<div class="user-img">
+                            <img src="${user.imageURL}" alt="imagem">
+                          </div>
+                          <div class="user-description">
+                            <p class="card-title">${user.name}</p>
+                            <p class="subtitle">${user.gender == "male" ? "Masculino" : "Feminino"} - ${user.age} Anos</p>
+                            <p class="subtitle">${user.cellPhone}</p>
+                            <p class="card-email">${user.email}</p>
+                          </div>`
+        document.querySelector('.user-list').appendChild(card)
+    })
+}
+
+loadUserData(26)
