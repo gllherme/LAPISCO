7277678a629226a0aa6510641acb8c04a68a34dd
gllherme <guicampos159@gmail.com>
2022-02-24 23:31:15 -0300

    final changes

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
+// Declara uma variável para a pagina que a API está carregando
+let page = 1
+
+// Declara uma array onde serão guardados os resultados da API
+const userData = []
+
+// Alterna a classe do html que define a visibilidade da animação de loading
+function toggleLoading(loading) {
+    const element = document.querySelector('.loading')
+    if (loading) {
+        element.classList.remove('hidden')
+    } else {
+        element.classList.add('hidden')
+    }
+}
+
 // Obtem os dados da API
 async function getUserData(numberOfElements) {
-    const response = await fetch(`https://randomuser.me/api/?results=${numberOfElements}`)
+    toggleLoading(true)
+    const response = await fetch(`https://randomuser.me/api/?results=${numberOfElements}&page=${page}`)
     const result = await response.json()
+    toggleLoading(false)
     return result["results"]
 }
 
-// Carrega e filtra os dados da API
+// Carrega e filtra os dados da API e guarda na array "userData"
 async function loadUserData(numberOfElements) { 
-    let userDataJson = await getUserData(numberOfElements)
-    
-    userDataJson.forEach(user => {
-        const name = `${user["name"]["first"]} ${user["name"]["last"]}`
-        const gender = user["gender"]
-        const cellPhone = user["cell"]
-        const email = user["email"]
-        const age = user["registered"]["age"]
-        const imageURL = user["picture"]["medium"]
-        const filteredUserData = {name, gender, cellPhone, email, age, imageURL}
-        userData.push(filteredUserData)
-    });
-    renderUserCard(userData)
+    const userDataJson = await getUserData(numberOfElements)
+    const newUser = userDataJson.map((user, index) => ({
+        id: `${page}-${index}`,
+        name: `${user.name.first} ${user.name.last}`,
+        gender: user.gender,
+        cellPhone: user.cell,
+        email: user.email,
+        age: user.registered.age,
+        imageURL: user.picture.medium
+        
+    }));
+    renderUserCard(newUser)
+    userData.push(...newUser)
 }
-
-let userData = []
 
 // Cria um elemento HTML contendo as informações geradas pela API
 function renderUserCard(userData) {
     userData.forEach(user => {
         const card = document.createElement('div')
-        console.log(card)
-        card.classList.add('user-card')
+        card.classList.add(`user-card`)
         card.innerHTML = `<div class="user-img">
                             <img src="${user.imageURL}" alt="imagem">
                           </div>
@@ -39,15 +54,39 @@ function renderUserCard(userData) {
                             <p class="subtitle">${user.cellPhone}</p>
                             <p class="card-email">${user.email}</p>
                           </div>`
+        card.setAttribute('id', user.id)
         document.querySelector('.user-list').appendChild(card)
     })
 }
 
-loadUserData(26)
-
+// Scroll infinito para gerar mais resultados
 window.addEventListener('scroll', () => {
-    if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
-        loadUserData(26)
+    if (window.scrollY + (window.innerHeight + 15) >= document.documentElement.scrollHeight) {
+        page++
+        loadUserData(16)
     }
 })
 
+// Sistema de busca
+function searchUserByName(name) {
+    return userData.filter((user) => {
+        return user.name.toLowerCase().includes(name.toLowerCase())
+    })
+}
+
+// Filtra a busca e define a visibilidade dos cards não relevantes por meio do CSS
+const searchElement = document.getElementById('search')
+searchElement.value = ""
+searchElement.onkeyup = () => {
+    const filteredSearch = searchUserByName(searchElement.value).map((user) => user.id)
+    const allUserCards = document.querySelectorAll('.user-card')
+    Array.from(allUserCards).map((card) => {
+        if (filteredSearch.includes(card.getAttribute('id'))) {
+            card.classList.remove('hidden')
+        } else {
+            card.classList.add('hidden')
+        }
+    })
+}
+
+loadUserData(16)
